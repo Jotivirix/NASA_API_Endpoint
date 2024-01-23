3b7ceefdb285a782264d198de011067d4b11db3e
Jotivirix <joignasa@gmail.com>
2024-01-23 18:31:39 +0100

    Some test created

---
+++ NASA_API_Endpoint_Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NASA_API_Endpoint.Models;
+
+namespace NASA_API_Endpoint_Tests;
+
+public class Tests
+{
+    AsteroidModel asteroidTest;
+
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var mock = new Mock<AsteroidModel>();
+
+        var mockObject = mock.Object;
+
+        mockObject.Diametro = 2.0;
+
+        Assert.That(mockObject.Diametro == 2.0);
+    }
+}
